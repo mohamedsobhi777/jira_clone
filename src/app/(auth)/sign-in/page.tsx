c618356496a4aba8af0db8956056aca4dd5cbe1b
msmhmorsi <msmhmorsi@connect.ust.hk>
2024-10-20 15:32:59 +0800

    auth pages

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+import React from 'react'
+
+import SignInCard from '@/features/auth/components/sign-in-card'
+
+type Props = {}
+
+const Page = (props: Props) => {
+    return <SignInCard />
+}
+
+export default Page
